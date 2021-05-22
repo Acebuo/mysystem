d23cac8bd7c0ff1786e5719dcfa42725b50f48e6
Ace <glen90803@gmail.com>
2021-05-22 23:55:49 +0800

    新增使用者名稱

--- register.js
+++ register.js
@@ -2,14 +2,21 @@ let registerEmail = document.getElementById('registerEmail')
 let registerPassword =document.getElementById('registerPassword')
 let registerBtn = document.getElementById('registerBtn')
 let registermodel = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+let registername =document.getElementById('registername')
+let userid;
 
 
 registerBtn.addEventListener('click',function(e){
 e.preventDefault();
+userid = registername.value;
 firebase.auth().createUserWithEmailAndPassword(registerEmail.value,registerPassword.value).then(()=>{
-    alert('註冊成功');
+    let user = firebase.auth().currentUser;
+    user.updateProfile({
+        displayName:userid
+    });
     registerEmail.value="";
     registerPassword.value="";
+    registername.value="";
     registermodel.toggle()
     }).catch((error)=>{alert(error.message); 
         registerEmail.value="";
--- signin.js
+++ signin.js
@@ -1,6 +1,7 @@
-let signinEmail = document.getElementById('signinEmail')
-let signinPassword = document.getElementById('signinPassword')
-let signinBtn = document.getElementById('signinBtn')
+let signinEmail = document.getElementById('signinEmail');
+let signinPassword = document.getElementById('signinPassword');
+let signinBtn = document.getElementById('signinBtn');
+let second = document.getElementById('second');
 
 
 signinBtn.addEventListener('click', function (e) {
@@ -8,9 +9,17 @@ signinBtn.addEventListener('click', function (e) {
     firebase.auth().signInWithEmailAndPassword(signinEmail.value, signinPassword.value).then(() => {
         let user = firebase.auth().currentUser;
         if (user) {
-            alert('登入成功');
+            document.getElementById('digimon').classList.add('none');
+            second.classList.remove('none');
+            let email = user.email;
+            let id = user.uid
+            document.getElementById('user-mail').innerHTML = `您的帳號:${email}`
+            document.getElementById('user-id').innerHTML = `您的id:${id}`
+            document.getElementById('user-name').innerHTML = `您的名稱:${user.displayName}`
+            console.log(user)
         }
     }).catch((error) => {
         alert(error.message);
     })
 });
+
